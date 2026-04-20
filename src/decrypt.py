c14495d2978e049fe936d2d1d9965ec4db631ab5
Bilel Majdoub <bilelmajdoub@MacBook-Air-de-Bilel.local>
2026-04-20 12:48:48 +0200

    feat:decrypt.py

---
+++ src/decrypt.py
@@ -0,0 +1,23 @@
+import decrypt_letter
+def decrypt(text,key):
+    """dechiffre un texte complet"""
+
+
+    res = []
+
+    for c in text:
+        if c.isalpha():
+            decrypted_char = decrypt_letter.decrypt_letter(c,key)
+            res.append(decrypted_char)
+        else:
+            res.append(c)
+
+    return ''.join(res)
+
+
+# jeux de tests
+
+print(decrypt("KHOOR",3))
+print(decrypt("BCD",1))
+print(decrypt("ABC",3))
+print(decrypt("GTSOTZW !",5))
